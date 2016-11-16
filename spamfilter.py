52d699b2ee2f291553952697fa5b11230b648ced
kavish jain <kavishme@gmail.com>
2016-11-15 18:19:40 -0800

    output fomatting

--- spamfilter.py
+++ spamfilter.py
@@ -21,6 +21,8 @@ def main():
 	sdict = getSpamDictionary(DICTFILE)
 	
 	docs = OrderedDict()
+	idf = OrderedDict()
+
 	for fi in os.listdir(MAILFOLDER):
 		if fi.endswith(".txt"):
 			f = open(MAILFOLDER + fi, 'r')
@@ -43,8 +45,6 @@ def main():
 	for doc in docs:
 		if 'tf' not in docs[doc].keys():
 			docs[doc]['tf'] = OrderedDict()
-		if 'idf' not in docs[doc].keys():
-			docs[doc]['idf'] = OrderedDict()
 		for word in sdict:
 			count = docs[doc]['count'][word]
 			if count:
@@ -52,33 +52,51 @@ def main():
 			else:
 				docs[doc]['tf'][word] = 0
 
-			freq = 0
-			for d in docs:
-				if docs[d]['count'][word] > 0:
-					freq += 1
-			if freq:
-				docs[doc]['idf'][word] = log((1+len(docs))/freq, 10)
-			else:
-				docs[doc]['idf'][word] = 0
 
-	print('-'*SEP)
-	print('Spam dictionary:', ','.join(sdict))
+	for word in sdict:
+		freq = 0
+		for d in docs:
+			if docs[d]['count'][word] > 0:
+				freq += 1
+		if freq:
+			idf[word] = log((1+len(docs))/freq, 10)
+		else:
+			idf[word] = 0
 
-	print('-'*SEP)
-	print('IDF')
+
 	for doc in docs:
-		print("\t".join([str(format(docs[doc]['idf'][word],fformat)) for word in docs[doc]['idf']]))
-		break;
+		if 'tfidf' not in docs[doc].keys():
+			docs[doc]['tfidf'] = OrderedDict()
+		for word in sdict:
+			docs[doc]['tfidf'][word] = idf[word] * docs[doc]['tf'][word]
+
 
 	print('-'*SEP)
-	print('Word frequency table')
-	for doc in docs:
-		print(doc, "\t".join([str(docs[doc]['count'][word]) for word in docs[doc]['count']]))
+	print('***Spam dictionary***', ','.join(sdict), sep='\n\n')
 
 	print('-'*SEP)
-	print('TF table')
+	print('***Word frequency table***')
+	print('\n')
 	for doc in docs:
-		print(doc, "\t".join([str(format(docs[doc]['tf'][word], fformat)) for word in docs[doc]['tf']]))
+		print(doc, " \t".join([str(docs[doc]['count'][word]) for word in docs[doc]['count']]))
+
+
+	print('-'*SEP)
+	print('***IDF***')
+	print('\n')
+	print("  \t".join([str(format(idf[word],fformat)) for word in sdict]))
+
+	print('-'*SEP)
+	print('***TF table***')
+	print('\n')
+	for doc in docs:
+		print(doc, " \t".join([str(format(docs[doc]['tf'][word], fformat)) for word in docs[doc]['tf']]))
+
+	print('-'*SEP)
+	print('***TF-IDF table***')
+	print('\n')
+	for doc in docs:
+		print(doc, " \t".join([str(format(docs[doc]['tfidf'][word], fformat)) for word in docs[doc]['tfidf']]))
 	print('-'*SEP)
 
 if __name__ == '__main__':
